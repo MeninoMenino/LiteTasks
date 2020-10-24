4a6567560f9c142cbcfcdb1d5776c13df9b7262c
Gabriel Menino <grmenino@gmail.com>
2020-10-24 11:23:55 -0300

    Botão, background e começo da listagem

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar, 
+  TouchableOpacity, FlatList } from 'react-native';
+import {Ionicons} from '@expo/vector-icons'
+import { TaskList } from './src/components/TaskList'
 
 export default function App() {
+
+  const [task, setTask] = useState([
+    {key: 1, task: 'Comprar pão'},
+    {key: 2, task: 'Comprar ovo'},
+    {key: 3, task: 'Comprar leite'},
+    {key: 4, task: 'Comprar carne'},
+    {key: 5, task: 'Comprar frutas'}
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#171d31" barStyle="light-content"/>
+
+      <View style={styles.content}>
+        <Text style={styles.title}> Tarefas </Text>
+      </View>
+
+      {/* 
+      
+      -----TODO: Corrigir a listagem.-----
+
+      <FlatList
+      marginHorizontal = {10}
+      showsHorizontalScrollIndicator={false}
+      data={task}
+      keyExtractor={ (item) => String(item.key)}
+      renderItem={ ({item}) => <TaskList data={item}/> }
+      />
+
+      */}
+
+      <TouchableOpacity style={styles.fab}>
+        <Ionicons name="ios-add" size={35} color="#FFF"/>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#171d31',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  content: {
+
+  },
+  title: {
+    marginTop: 10,
+    paddingBottom: 10,
+    fontSize: 25,
+    textAlign: "center",
+    color: "#FFF"
+  },
+  fab: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    backgroundColor: '#0094ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    right: 25,
+    bottom: 25,
+    elevation: 2,
+    zIndex: 9,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 1,
+      height: 3
+    }
+  }
 });
---
+++ src/components/TaskList/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function TaskList(){
+    return(
+        <View style={styles.container}>
+            <Text style={{fontSize: 30}}>...</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+});
