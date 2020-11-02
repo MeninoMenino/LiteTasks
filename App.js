f9aa3d1ff6f896e9af0a5abdac7f85fcf2166a51
Gabriel Menino <grmenino@gmail.com>
2020-11-02 11:52:32 -0300

    Criação da pasta styles com arquivos de estilo

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar, 
+import { Text, View, SafeAreaView, StatusBar, 
   TouchableOpacity, FlatList, Modal, TextInput, AsyncStorage } from 'react-native';
+
 import {Ionicons} from '@expo/vector-icons'
-import TaskList from './src/components/TaskList'
 import * as Animatable from 'react-native-animatable';
+
+import TaskList from './src/components/TaskList'
+import styles from './src/styles/App'
 
 //Botão customizado animado
 const AnimatedBtn = Animatable.createAnimatableComponent(TouchableOpacity);
@@ -137,82 +140,3 @@ export default function App() {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#8c8c8c'
-  },
-  content: {
-
-  },
-  title: {
-    marginTop: 10,
-    paddingBottom: 10,
-    fontSize: 25,
-    textAlign: "center",
-    color: "#FFF"
-  },
-  fab: {
-    position: 'absolute',
-    width: 60,
-    height: 60,
-    backgroundColor: '#4d4d4d',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 30,
-    right: 25,
-    bottom: 25,
-    elevation: 2,
-    zIndex: 9,
-    shadowColor: '#000',
-    shadowOpacity: 0.2,
-    shadowOffset: {
-      width: 1,
-      height: 3
-    }
-  },
-  modal: {
-    flex: 1,
-    backgroundColor: '#8c8c8c'
-  },
-  modalHeader: {
-    marginLeft: 10,
-    marginTop: 20,
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  modalTitle: {
-    marginLeft: 15,
-    fontSize: 23,
-    color: '#FFF'
-  },
-  modalBody: {
-    marginTop: 15
-  },
-  input: {
-    fontSize: 15,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 30,
-    backgroundColor: '#FFF',
-    padding: 9,
-    height: 85,
-    textAlignVertical: 'top',
-    color: '#000',
-    borderRadius: 5
-  },
-  handleAdd: {
-    backgroundColor: '#FFF',
-    marginTop: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginLeft: 10,
-    marginRight: 10,
-    height: 40,
-    borderRadius: 40
-  },
-  handleAddText: {
-    fontSize: 20
-  },
-});
---
+++ src/styles/App.js
@@ -0,0 +1,82 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#8c8c8c'
+    },
+    content: {
+  
+    },
+    title: {
+      marginTop: 10,
+      paddingBottom: 10,
+      fontSize: 25,
+      textAlign: "center",
+      color: "#FFF"
+    },
+    fab: {
+      position: 'absolute',
+      width: 60,
+      height: 60,
+      backgroundColor: '#4d4d4d',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 30,
+      right: 25,
+      bottom: 25,
+      elevation: 2,
+      zIndex: 9,
+      shadowColor: '#000',
+      shadowOpacity: 0.2,
+      shadowOffset: {
+        width: 1,
+        height: 3
+      }
+    },
+    modal: {
+      flex: 1,
+      backgroundColor: '#8c8c8c'
+    },
+    modalHeader: {
+      marginLeft: 10,
+      marginTop: 20,
+      flexDirection: 'row',
+      alignItems: 'center'
+    },
+    modalTitle: {
+      marginLeft: 15,
+      fontSize: 23,
+      color: '#FFF'
+    },
+    modalBody: {
+      marginTop: 15
+    },
+    input: {
+      fontSize: 15,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 30,
+      backgroundColor: '#FFF',
+      padding: 9,
+      height: 85,
+      textAlignVertical: 'top',
+      color: '#000',
+      borderRadius: 5
+    },
+    handleAdd: {
+      backgroundColor: '#FFF',
+      marginTop: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginLeft: 10,
+      marginRight: 10,
+      height: 40,
+      borderRadius: 40
+    },
+    handleAddText: {
+      fontSize: 20
+    },
+  });
+  
+  export default styles;
